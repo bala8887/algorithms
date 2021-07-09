2feb588afd25a715218a885fb3137e9c6d3195f8
Balachandar Paulraj <45397262+bala8887@users.noreply.github.com>
2021-07-08 22:42:26 -0700

    Update binarySearch.scala

--- binarySearch.scala
+++ binarySearch.scala
@@ -1,6 +1,7 @@
   def binarySearch(a : List[Int], givenNo : Int) : Unit = {
     //Need to set up counter to know the number of iterations
     //Should check the givenNo with minimum and maximum value
+    //Below code needs to be accommodate element present in zeroth index
     val med = a.length / 2
     if (med < 1) {
       println("exit")
